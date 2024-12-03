08032584797b4368c2fa0eed4a02c1d64b3bce32
melanie <mjharv@hotmail.com>
2024-12-03 14:34:20 -0700

    artwork model

---
+++ src/models/Artwork.js
@@ -0,0 +1,13 @@
+export class Artwork {
+constructor(data) {
+    this.id = data.id
+    this.color = data.color
+    this.height = data.height
+    this.imgUrl = data.imgUrl
+    this.description = data.description
+    this.slug = data.slug
+    this.width = data.width
+    this.attribution = data.attribution
+}
+
+}
